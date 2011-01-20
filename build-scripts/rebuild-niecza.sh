cf1752c1846ef6a71273f33f2e0fed6d0e30520a
Perl 6 Evalbot <p6eval@host04.(none)>
2011-01-20 17:57:55 +0000

    fixes for niecza rebuild & p6eval

--- build-scripts/rebuild-niecza.sh
+++ build-scripts/rebuild-niecza.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATH=$PATH:/usr/local/bin
-PATH=/usr/local/bin:/usr/bin:/bin:/usr/games
+PATH=/home/p6eval/sprixel/clr/bin:/usr/local/bin:/usr/bin:/bin:/usr/games
 echo "updating niecza"
 cd ~/niecza/
 git pull
