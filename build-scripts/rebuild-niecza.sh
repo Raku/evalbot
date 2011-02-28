762d62c28e0d9753a101abaf19a8d3e81e343aa2
Moritz Lenz <moritz@faui2k3.org>
2011-02-28 22:48:24 +0100

    [niecza build] use mono 2.10.0

--- build-scripts/rebuild-niecza.sh
+++ build-scripts/rebuild-niecza.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-PATH=$PATH:/usr/local/bin
-PATH=/home/p6eval/sprixel/clr/bin:/usr/local/bin:/usr/bin:/bin:/usr/games
+PATH=$PATH:/usr/local/mono-2.10.1/bin
+#PATH=/home/p6eval/sprixel/clr/bin:/usr/local/bin:/usr/bin:/bin:/usr/games
 echo "updating niecza"
 cd ~/niecza/
 git pull
