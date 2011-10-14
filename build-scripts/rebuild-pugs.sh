c1461f2e89d11ef1c2db81a3940d4baa335eae2a
Moritz Lenz <moritz@faui2k3.org>
2011-10-14 08:44:57 +0200

    [pugs] improved sanity

--- build-scripts/rebuild-pugs.sh
+++ build-scripts/rebuild-pugs.sh
@@ -2,11 +2,13 @@
 cd
 set -e
 echo -e "\nNew pugs build"
+GHCDIR="$HOME/ghc-7.2.1"
+PATH="$GHCDIR/bin":$PATH
 date
 cd ~/Pugs.hs/
 git pull
 cd Pugs
 make
-make INSTALLBIN=$HOME/ghc-7.2.1/bin/ install
-git log -1 --pretty="format:%h" >  $HOME/ghc-7.2.1/pugs_version
+make INSTALLBIN="$GHCDIR/bin"/ install
+git log -1 --pretty="format:%h" >  $GHCDIR/pugs_version
 echo
