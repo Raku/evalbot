fc7f87419d18ded6f4ae341cc7ae22478b4cfbac
Moritz Lenz <moritz.lenz@gmail.com>
2019-10-01 19:52:10 +0200

    Avoid concurrent rebuilds

--- build.pl
+++ build.pl
@@ -6,6 +6,7 @@ use warnings;
 use autodie;
 use List::Util qw(first);
 use FindBin;
+use Fcntl qw(:DEFAULT :flock);
 
 my $what = shift(@ARGV) or die "Usage: $0 <project>\n";
 
@@ -13,7 +14,13 @@ my $script_dir = "$FindBin::Bin/build-scripts/";
 my $script = first { -e $_ } "$script_dir/rebuild-$what.pl", "$script_dir/rebuild-$what.sh";
 
 die "Found no rebuild script for $what\n" unless $script;
+open my $lock_file, '>', "$script.lock"
+    or die "Cannot open lock $script.lock: $!";
+flock( $lock_file, LOCK_EX | LOCK_NB )
+    or die "Cannot lock file $script.lock: $!";
 
 system "flock -w 60 $script_dir/lock.$what $script @ARGV >~/log/$what.log 2>&1";
-unlink "$script_dir/lock.$what";
+flock ( $lock_file, LOCK_UN );
+close $lock_file;
+unlink "$script.lock";
 #system $^X, "$FindBin::Bin/sync.pl",  $what;
